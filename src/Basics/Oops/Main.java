eaa80de4b85bacd5dd87582a11ed9a4e519474d2
johnsuneel <suneelvangalapati93@gmail.com>
2026-05-06 17:35:40 +0530

    New changes1

---
+++ src/Basics/Oops/Main.java
@@ -0,0 +1,9 @@
+package Basics.Oops;
+
+public class Main {
+    public static void main(String[] args) {
+        Overloading ol= new Overloading();
+        ol.m1();
+        ol.m2();
+    }
+}
--- src/Basics/Oops/Overloading.java
+++ src/Basics/Oops/Overloading.java
@@ -1,4 +1,11 @@
 package Basics.Oops;
 
 public class Overloading {
+    public void m1(){
+        System.out.println("From m1()");
+    }
+    public void m2(){
+        System.out.println("From m2()");
+    }
+
 }
