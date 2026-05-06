cf50aeb140f17fb3b28b57f3269a42944b397724
johnsuneel <suneelvangalapati93@gmail.com>
2026-05-06 17:45:41 +0530

    correction Updates

--- src/Basics/Oops/Main.java
+++ src/Basics/Oops/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Overloading ol= new Overloading();
         ol.m1();
-        ol.m2();
+        ol.m2(10);
+        ol.m3(10L);
     }
 }
--- src/Basics/Oops/Overloading.java
+++ src/Basics/Oops/Overloading.java
@@ -4,8 +4,11 @@ public class Overloading {
     public void m1(){
         System.out.println("From m1()");
     }
-    public void m2(){
+    public void m2(int i){
         System.out.println("From m2()");
+    }
+    public void m3(float f){
+        System.out.println("From m3()");
     }
 
 }
